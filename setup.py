a56c080b498f79a3ed119c28ef339674a34724a2
jdonic <jdoni@jdonic.fedora>
2023-01-09 21:01:00 +0100

    Automate testing and lynting
    Setup automation for testing and lynting of the entire project using tox tool.
    For seperation of dependencies beetween dev and prod, the requirementes-dev.txt was created.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tasks",
+    version="1.0.0",
+    author="Jakub Donič",
+    author_email="donicjakub@gmail.com",
+    description=("A Django app for creating and viewing tasks"),
+    url="https://github.com/jdonic/tasks",
+)
