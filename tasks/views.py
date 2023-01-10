8723423e70585532878d64584a6f076eb380ea2c
jdonic <jdoni@jdonic.fedora>
2023-01-10 02:10:01 +0100

    Implement task detail
    Implement the url, view and basic template for
    task detail

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,15 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Task
 
 class TaskListView(ListView):
     model = Task
+    context_object_name = "task_list"
     template_name = "tasks/task_list.html"
     ordering = ["due_date"]
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    context_object_name = "task"
+    template_name = "tasks/task_detail.html"
