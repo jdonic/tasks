c6291c6fc75b537c8c96d2eedf2590b80a2b94af
jdonic <jdoni@jdonic.fedora>
2023-01-10 01:29:46 +0100

    Create test for task_list
    Create test suite for task_list url. Test that the tasks are ordered by
    due_date from most imminent to the least one.

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
-from .models import Task
+from django.urls import reverse
 from django.utils import timezone
 
 import datetime
 
+from .models import Task
 
 class TaskTest(TestCase):
     @classmethod
@@ -11,14 +12,24 @@ class TaskTest(TestCase):
         cls.task = Task.objects.create(
             title="Fix the car",
             description="Fix the car so I can go to Croatia.",
-            due_date=datetime.date(1997, 10, 19),
+            due_date=datetime.date(2024, 10, 19),
+        )
+        cls.task_2 = Task.objects.create(
+            title="Fix the washing_machine.",
+            description="Fix washing machine so I have something to wear.",
+            due_date=datetime.date(2023, 10, 19),
+        )
+        cls.task_3 = Task.objects.create(
+            title="Fix myself",
+            description="So I am not such a mess",
+            due_date=datetime.date(2025, 10, 19),
         )
 
     def test_task_model(self) -> None:
         self.assertEqual(f"{self.task}", "Fix the car")
         self.assertEqual(self.task.title, "Fix the car")
         self.assertEqual(self.task.description, "Fix the car so I can go to Croatia.")
-        self.assertEqual(self.task.due_date, datetime.date(1997, 10, 19))
+        self.assertEqual(self.task.due_date, datetime.date(2024, 10, 19))
         self.assertFalse(self.task.is_completed)
         self.assertIsNone(self.task.completed_at)
 
@@ -29,3 +40,14 @@ class TaskTest(TestCase):
         self.assertAlmostEqual(
             self.task.completed_at, timezone.now(), delta=timezone.timedelta(seconds=1)
         )
+
+    def test_task_list_view(self) -> None:
+        response = self.client.get(reverse("task_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "tasks/task_list.html")
+        self.assertContains(response, "Fix the car")
+        self.assertContains(response, "Fix myself")
+        tasks = response.context["object_list"]
+        self.assertEqual(tasks[0], self.task_2)
+        self.assertEqual(tasks[1], self.task)
+        self.assertEqual(tasks[2], self.task_3)
