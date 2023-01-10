4b1ddb0159c6e8e6e4c50ef9ef54f466d5031175
jdonic <jdoni@jdonic.fedora>
2023-01-10 02:38:03 +0100

    Add creation of a task
    Add url, view and basic template for creating a new task

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Task
+
 
 class TaskListView(ListView):
     model = Task
@@ -13,3 +14,10 @@ class TaskDetailView(DetailView):
     model = Task
     context_object_name = "task"
     template_name = "tasks/task_detail.html"
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    context_object_name = "task_new"
+    template_name = "tasks/task_new.html"
+    fields = ["title", "description", "due_date"]
